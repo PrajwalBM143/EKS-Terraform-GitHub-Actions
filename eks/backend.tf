e59fba17bb03e714924e01c1d616b37bbf49652e
Prajwal BM <77157069+PrajwalBM143@users.noreply.github.com>
2026-08-09 11:56:54 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "prajwal-s3-8099"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
